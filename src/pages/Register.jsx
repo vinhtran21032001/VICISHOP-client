4bb8533b392b66326ee5182a2f7b444f7885e868
jiren <vinhtran21032001@gmail.com>
2021-11-20 17:11:38 +0700

    update open

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -206,8 +206,6 @@ const Register = () => {
         })
 
 
-
-
         const onSubmit =  async (values, {resetForm }) =>{
             const {confirmpassword,...data} = values;
             dispatch(registerStart());
